d4e726612766eb110f34c66de8bef31542475262
Avinash Singh <avs2805@gmail.com>
2020-05-18 08:55:54 -0700

    added comments

--- project1_solution/scripts/solution.py
+++ project1_solution/scripts/solution.py
@@ -1,31 +1,30 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 import rospy
 
 from std_msgs.msg import Int16
 from project1_solution.msg import TwoInts
 
+
 def two_ints_callback(data, my_pub):
+    # extract data from message
     int_a = data.a
     int_b = data.b
 
+    # sum ints
     sum_ints = data.a + data.b
 
-    rospy.loginfo(rospy.get_caller_id() + "the two ints are %d and %d, and their sum is: %d", int_a, int_b, sum_ints)
+    rospy.loginfo(rospy.get_caller_id(
+    ) + "the two ints are %d and %d, and their sum is: %d", int_a, int_b, sum_ints)
 
     # publish sum of two ints to the publisher
     my_pub.publish(sum_ints)
 
 
-def two_sum():
-    pass
-   
-
-
-if __name__ == "__main__":
+def twoint_listener():
      # initialize node
-    rospy.init_node('two_sum_publisher',anonymous=True)
-    
-    # declare publisher object from publisher class, topicName, msgType, queue sizee
+    rospy.init_node('two_sum_publisher', anonymous=True)
+
+    # declare publisher object from publisher class, topicName, msgType, queue size
     my_pub = rospy.Publisher('sum', Int16, queue_size=10)
 
     # subscribe to two_ints
@@ -33,4 +32,9 @@ if __name__ == "__main__":
 
     rospy.spin()
 
-   
+
+if __name__ == "__main__":
+    try:
+        twoint_listener()
+    except rospy.ROSInterruptException:
+        pass
